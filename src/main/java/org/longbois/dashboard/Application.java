9e7a9d19d9d209c889b4bd5da3edb91cdbaccbc2
Fyxren <benweerd@gmail.com>
2024-06-18 22:35:03 +0200

    feat: more 'minor' changes

--- src/main/java/org/longbois/dashboard/Application.java
+++ src/main/java/org/longbois/dashboard/Application.java
@@ -11,8 +11,8 @@ import java.util.Objects;
 public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-//        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("/org/longbois/dashboard/login/Login.fxml"));
-        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("/org/longbois/dashboard/home/Home.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("/org/longbois/dashboard/login/Login.fxml"));
+//        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("/org/longbois/dashboard/home/Home.fxml"));
 
         Scene scene = new Scene(fxmlLoader.load(), 1380, 810);
         Image iconStream = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/icons/lb-logo.png")));
--- src/main/java/org/longbois/dashboard/newSensor/NewSensorController.java
+++ src/main/java/org/longbois/dashboard/newSensor/NewSensorController.java
@@ -77,11 +77,11 @@ public class NewSensorController {
         sensorData.put("depth", sensorDepth);
         sensorData.put("type", sensorType.toLowerCase());
         sensorData.put("unit", sensorUnit);
-        sensorData.put("station_id", 1);
+        sensorData.put("stationId", 1);
 
-        System.out.println(sensorData);
-
-        JSONObject response = apiService.postData("http://bweerd.gcmsi.nl/api/sensors", sensorData.toString());
+        System.out.println("JSONObject" + sensorData);
+//        String jsonObject = "{\"name\":\"" + sensorName + "\",\"depth\":" + sensorDepth + ",\"type\":\"" + sensorType.toLowerCase() + "\",\"unit\":\"" + sensorUnit + "\",\"station_id\":1}";
+        JSONObject response = apiService.postData("http://bweerd.gcmsi.nl/api/sensors", jsonObject);
         if (response == null) {
             System.out.println("API - Error while posting data");
             return;
